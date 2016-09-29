0b2355867cdd97c330b80e2219cbb82ad66850d1
inzamam malik <malikasinger@gmail.com>
2016-09-29 21:15:25 +0500

    login is implemented

--- mongodb-connection/handler.js
+++ mongodb-connection/handler.js
@@ -53,17 +53,51 @@ module.exports.login = (event, context, cb) => {
     var username = event.body.username;
     var password = event.body.password;
 
-    // newUser.findOne({ username: username, password: password }, function (err, data) {
-    //     if (err) {
-    //         console.log("error: ", err);
-    //     } else {
-    //         console.log("data: ", data);
-    //     }
-    //     cb(null, {
-    //         message: 'function executed successfully - POST!',
-    //         event
-    //     });
-    // });
+    models.user.findOne({ username: username }, function (err, data) {
+        if (err) {
+            console.log("some error occured in findOne: ", err);
+            cb(null, {
+                message: 'some error occured in findOne!',
+                event
+            });
+        } else {
+            console.log("user found in database, checking password");
+
+            encryption.varifyHash(password, data.password).then(function (success) {
+
+                console.log("password is correct, generating token...");
+                //generate token here and send to user
+                auth.generateToken({ username: data.username }).then(function (token) {
+
+                    console.log("logged in successfully, token: ", token);
+                    cb(null, {
+                        message: 'logged in successfully, token: ' + token,
+                        event
+                    });
+
+                }, function (err) {
+                    console.log("error in token generation: ", err);
+                    cb(null, {
+                        message: 'error in token generation',
+                        event
+                    });
+                });
+
+            }, function (err) {
+                console.log("password not matched", err);
+                cb(null, {
+                    message: "password not matched",
+                    event
+                });
+            });
+        }
+    })
+
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////
+module.exports.profile = (event, context, cb) => {
+    //verify token here
+
+};
+/////////////////////////////////////////////////////////////////////////////////////
