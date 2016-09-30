c9fe0500abac7432096df58c44ff00a93bb54c4b
inzamam malik <malikasinger@gmail.com>
2016-09-30 17:16:16 +0500

    add schema

--- mongodb-connection/db/schema.js
+++ mongodb-connection/db/schema.js
@@ -2,11 +2,78 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 exports.userSchema = new Schema({
-    username: String, //this will contain company identification of which this order is related
-    password: String, // this will contain sale man identification who is placing this order    
+    
+        firstName: {
+            type: String
+        },
+
+        lastName: {
+            type: String
+        },
+
+        avatar: {
+            type: String
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true
+        },
+
+        phone: {
+            type: String,
+            required: true,
+            unique: true
+        },
+
+        password: {
+            type: String,
+            required: true,
+        },
+
+        role: {
+            type: String,
+            enum: ['user', 'stylist', 'admin', 'super'],
+            default: 'user',
+            required: true
+        },
+
+        fbId: {
+            type: String,
+            unique: 1
+        },
+
+        timeZone: {
+            type: Number,
+            default: -7
+        },
+
+        payment: {
+            stripeId: String,
+            status: {
+                type: String,
+                enum: ['unverified', 'verified', 'pending']
+            }
+        },
+
+        pushTokens: [{
+            type: String
+        }],
+
+        resetToken: {
+            type: String
+        },
+
+        active: {
+            type: Boolean,
+            required: true,
+            default: true
+        }  
 });
 
 exports.loggedInUsers = new Schema({
     hash: String,
-    username: String,        
+    email: String,        
 });
