d658c9dd137484f9c5bdd6510144c0256999baaf
inzamam malik <malikasinger@gmail.com>
2016-09-30 16:41:51 +0500

    a not handled condition is now handled in login function

--- mongodb-connection/handler.js
+++ mongodb-connection/handler.js
@@ -24,8 +24,6 @@ module.exports.signup = (event, context, cb) => {
     var password = event.body.password;
 
     encryption.stringToHash(password).then(function (passwordHash) {
-
-        console.log("abc");
         var newUser = new models.user({
             username: username,
             password: passwordHash
@@ -54,53 +52,49 @@ module.exports.login = (event, context, cb) => {
     var password = event.body.password;
 
     models.user.findOne({ username: username }, function (err, data) {
-        if (err) {
-            console.log("some error occured in findOne: ", err);
-            cb(null, {
-                message: 'some error occured in findOne!',
-                event
-            });
-        } else {
-            console.log("user found in database, checking password");
-
-            encryption.varifyHash(password, data.password).then(function (success) {
-                if (success) {
-                    console.log("password is correct, generating token...");
-                    //generate token here and send to user
-                    auth.generateToken({ username: data.username }).then(function (token) {
-
-                        console.log("logged in successfully, token: ", token);
-                        cb(null, {
-                            message: 'logged in successfully, token: ' + token,
-                            event
-                        });
-
-                    }, function (err) {
-                        console.log("error in token generation: ", err);
-                        cb(null, {
-                            message: 'error in token generation',
-                            event
-                        });
-                    });
-                } else {
-                    console.log("password is incorrect");
-                    cb(null, {
-                        message: "password is incorrect",
-                        event
-                    });
-                }
-
-
-            }, function (err) {
-                console.log("password not matched", err);
+        if (!err) {
+            if (!data) {
+                console.log("user not exist: ", err);
                 cb(null, {
-                    message: "password not matched",
+                    message: 'usernot exist!',
                     event
                 });
-            });
+            } else {
+                console.log("user found in database, checking password", data);
+                encryption.varifyHash(password, data.password).then(function (success) {
+                    if (success) {
+                        console.log("password is correct, generating token...");
+                        //generate token here and send to user
+                        auth.generateToken({ username: data.username }).then(function (token) {
+                            console.log("logged in successfully, token: ", token);
+                            cb(null, {
+                                message: 'logged in successfully, token: ' + token,
+                                event
+                            });
+                        }, function (err) {
+                            console.log("error in token generation: ", err);
+                            cb(null, {
+                                message: 'error in token generation',
+                                event
+                            });
+                        });
+                    } else {
+                        console.log("password is incorrect");
+                        cb(null, {
+                            message: "password is incorrect",
+                            event
+                        });
+                    }
+                }, function (err) {
+                    console.log("password not matched", err);
+                    cb(null, {
+                        message: "password not matched",
+                        event
+                    });
+                });
+            }            
         }
     })
-
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////
