3f530d869d80e58c395e9a77a2a2531448eb8f8d
inzamam malik <malikasinger@gmail.com>
2016-09-30 17:20:51 +0500

    indenting only

--- mongodb-connection/db/schema.js
+++ mongodb-connection/db/schema.js
@@ -2,78 +2,66 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 exports.userSchema = new Schema({
-    
-        firstName: {
-            type: String
-        },
 
-        lastName: {
-            type: String
-        },
-
-        avatar: {
-            type: String
-        },
-
-        email: {
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    avatar: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ['user', 'stylist', 'admin', 'super'],
+        default: 'user',
+        required: true
+    },
+    fbId: {
+        type: String,
+        unique: 1
+    },
+    timeZone: {
+        type: Number,
+        default: -7
+    },
+    payment: {
+        stripeId: String,
+        status: {
             type: String,
-            required: true,
-            unique: true,
-            lowercase: true
-        },
-
-        phone: {
-            type: String,
-            required: true,
-            unique: true
-        },
-
-        password: {
-            type: String,
-            required: true,
-        },
-
-        role: {
-            type: String,
-            enum: ['user', 'stylist', 'admin', 'super'],
-            default: 'user',
-            required: true
-        },
-
-        fbId: {
-            type: String,
-            unique: 1
-        },
-
-        timeZone: {
-            type: Number,
-            default: -7
-        },
-
-        payment: {
-            stripeId: String,
-            status: {
-                type: String,
-                enum: ['unverified', 'verified', 'pending']
-            }
-        },
-
-        pushTokens: [{
-            type: String
-        }],
-
-        resetToken: {
-            type: String
-        },
-
-        active: {
-            type: Boolean,
-            required: true,
-            default: true
-        }  
+            enum: ['unverified', 'verified', 'pending']
+        }
+    },
+    pushTokens: [{
+        type: String
+    }],
+    resetToken: {
+        type: String
+    },
+    active: {
+        type: Boolean,
+        required: true,
+        default: true
+    }
 });
 
 exports.loggedInUsers = new Schema({
     hash: String,
-    email: String,        
+    email: String,
 });
--- mongodb-connection/pureFunctions/encryption.js
+++ mongodb-connection/pureFunctions/encryption.js
@@ -4,15 +4,15 @@ var q = require("q");
 
 //this function will take a string as input and return hash on success and error object on error
 
-exports.stringToHash = function(PasswordString) {
+exports.stringToHash = function (PasswordString) {
 
     var deferred = q.defer();
     var round = 10;
-    bcrypt.genSalt(round, function(err, salt) {
+    bcrypt.genSalt(round, function (err, salt) {
         if (err) {
             deferred.reject(err);
         }
-        bcrypt.hash(PasswordString, salt, function() { }, function(err, hashedPassword) {
+        bcrypt.hash(PasswordString, salt, function () { }, function (err, hashedPassword) {
             if (err) {
                 deferred.reject(err);
             }
@@ -22,25 +22,25 @@ exports.stringToHash = function(PasswordString) {
     return deferred.promise;
 }
 
-exports.varifyHash = function(realPassword,hashString) {
+exports.varifyHash = function (realPassword, hashString) {
     var deferred = q.defer();
-    bcrypt.compare(realPassword,hashString,function(err,result){//'result'' will be boolean 
-        if(err){               
-           deferred.reject(err);//it means hash is invalid
-        }          
+    bcrypt.compare(realPassword, hashString, function (err, result) {//'result'' will be boolean 
+        if (err) {
+            deferred.reject(err);//it means hash is invalid
+        }
         deferred.resolve(result);//return with boolean 'Hash' is matched or not
-    }); 
+    });
     return deferred.promise;
 }
 
 
-exports.validateHash = function(hashString) {//true or false in resolve, no reject
+exports.validateHash = function (hashString) {//true or false in resolve, no reject
     var deferred = q.defer();
-    bcrypt.compare("dummy",hashString,function(err,result){//'result'' will be boolean 
-        if(err){               
-           deferred.resolve(false);//it means Hash is invalid
-        }          
+    bcrypt.compare("dummy", hashString, function (err, result) {//'result'' will be boolean 
+        if (err) {
+            deferred.resolve(false);//it means Hash is invalid
+        }
         deferred.resolve(true);//it means hash is either matched or not but it is a valid Hash
-    }); 
+    });
     return deferred.promise;
 }
