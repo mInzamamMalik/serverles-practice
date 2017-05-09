5716a0084591c56545b23eb9281558cb2bd2a795
Inzamam Malik <malikasinger1@gmail.com>
2017-05-10 01:35:19 +0500

    major bug resolved

--- latest/nodeServer.js
+++ latest/nodeServer.js
@@ -5,7 +5,7 @@ var VoiceResponse = require('twilio').twiml.VoiceResponse;
 var app = express();
 var port = (process.env.PORT || 4000);
 
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
 
 // helper to append a new "Say" verb with alice voice
 function say(text, twimlRef) {
@@ -18,28 +18,48 @@ function respond(responseRef, twimlRef) {
 }
 
 app.post("/voice", function (request, response, next) {
-    console.log("request.body: ", request.body); //body is comming as empty object
+    console.log("request.headers: ", request.headers);
     console.log("request.query: ", request.query);
+    console.log("request.body: ", request.body); //body is comming as empty object
 
     var phone = request.body.From;
     var input = request.body.RecordingUrl;
     var twiml = new VoiceResponse();
     console.log("phone, input: ", phone, input);
 
-    say('What type of podcast would you like to listen. Press any key to finish.', twiml);
+    say('What type of podcast would you like to listen', twiml);
     twiml.record({
         method: 'POST',
-        action: '/voice/transcribe',
-        transcribeCallback: '/voice/transcribe',
+        action: '/record/action',
+        transcribeCallback: '/record/transcribe',
         maxLength: 10
     });
+    respond(response, twiml);
+});
+
+app.post("/record/action", function (request, response, next) {
+    console.log("/record/action");
+    console.log("request.headers: ", request.headers);
+    console.log("request.query: ", request.query);
+    console.log("request.body: ", request.body); //body is comming as empty object
+
+    var twiml = new VoiceResponse();
+
+    say('please wait.', twiml);
+    twiml.pause();
+    twiml.redirect('/record/action');
 
     respond(response, twiml);
 });
 
-app.post("/voice/transcribe", function (request, response, next) {
-    console.log("request: ", request.body); //body is comming as empty object
+app.post("/record/transcribe", function (request, response, next) {
+
+    console.log("/record/transcribe");
+
+    console.log("request.headers: ", request.headers);
     console.log("request.query: ", request.query);
+    console.log("request.body: ", request.body); //body is comming as empty object
+
 
     var phone = request.body.From;
     var input = request.body.RecordingUrl;
@@ -48,10 +68,12 @@ app.post("/voice/transcribe", function (request, response, next) {
     var transcript = request.body.TranscriptionText;
 
     console.log("transcribe text: ", transcript);
-    var mp3Url = 'https://api.twilio.com/cowbell.mp3'
 
     say('start playing.', twiml);
-    twiml.play(mp3Url);
+    say(transcript, twiml);
+
+    // var mp3Url = 'https://api.twilio.com/cowbell.mp3'
+    // twiml.play(mp3Url);
 
     respond(response, twiml);
 });
